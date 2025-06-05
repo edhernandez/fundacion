4a42ef852f5788ec6e61e6ff84000633c8f965a2
Edwin Hernandez <edwin.hernandez@qualitservices.tech>
2025-06-05 09:22:00 -0500

    Se ajusta testimonios

--- src/components/home/TestimonialCarousel.tsx
+++ src/components/home/TestimonialCarousel.tsx
@@ -7,23 +7,38 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 const testimonials = [
     {
         rating: 4.9,
-        text: "Los programas de formación y el apoyo de la fundación son de gran calidad. Brindan herramientas y conocimientos clave para el desarrollo personal y profesional. Gracias a mentores y voluntarios, esta experiencia es invaluable. Sin duda, es una iniciativa transformadora que recomiendo a quienes buscan aprender y crecer.",
-        name: "Camila Andrea Moreno Estupiñan",
+        text: "HEINSOHN para mí ha sido llena de oportunidad y enseñanzas. Estoy agradecida con la compañía y, especialmente, con las personas que se toman el tiempo de enseñarme y compartir sus conocimientos. Ha representado una valiosa oportunidad para crecer tanto profesional como personalmente. Me enseña lo que es la resiliencia en todos sus entornos, me quede o me vaya heinsohn es el inicio de una vida de éxito.",
+        name: "Yuli Surley Saavedra Pérez",
     },
     {
         rating: 4.7,
-        text: "La fundación ha sido un gran apoyo en mi formación. Sus programas me han brindado conocimientos valiosos y nuevas oportunidades. La dedicación de los mentores hace que aprender sea motivador y accesible. Sin duda, es una iniciativa que cambia vidas y recomiendo completamente su labor.",
-        name: "Edwin Andres Parra Morales",
+        text: "Mi experiencia en Heinsohn ha sido muy valiosa para mí, a lo largo del proceso, pude aprender cosas nuevas, desarrollar habilidades que antes no había explorado y crecer a nivel personal y laboral, me siento agradecida por la oportunidad y por todo el aprendizaje que me llevo, está experiencia me motiva a seguir mejorando.",
+        name: "Karol Valentina Muñoz Bocanegra",
     },
     {
         rating: 4.8,
-        text: "Gracias a la fundación, he adquirido habilidades que antes parecían inalcanzables. Su enfoque en la educación y la tecnología abre puertas y transforma realidades. Los voluntarios hacen que cada experiencia sea enriquecedora. Es un programa que realmente impulsa el crecimiento personal y .",
-        name: "Juan Jose Vaez Naranjo",
+        text: "La experiencia en Heinsohn para mí ha sido una puerta de oportunidad, una etapa de aprendizaje y retos contantes donde eh fortalecido mis conocimientos en diferentes áreas, lo cual ha contribuido en mi continuo desarrollo como futura profesional y me ha ayudado a fortalecer mi área personal.",
+        name: "María Fernanda Benavides Cajamarca",
     },
     {
         rating: 4.6,
-        text: "Me ha permitido desarrollarme profesionalmente...",
-        name: "Laura Fernanda Pérez",
+        text: "Mi experiencia en Heinsohn ha sido llena de oportunidades, de nuevas experiencias que me han ayudado en mi nivel académico y laboral, agradezco al equipo con el que me toco porque cada día tuvieron la iniciativa y la disposición para explicarme y poder aprender. Es un gran aprendizaje el que me llevo de esta empresa, tanto las personas como el ambiente laboral me hacen sentir en mi como en mi casa. Agradezco a la empresa Heinsohn por abrirme sus puertas y motivarme cada dia a ser mejor.",
+        name: "Daniel David Barrera Barazarte",
+    },
+    {
+        rating: 4.5,
+        text: "En mi experiencia en Heinsohn ha sido una gran oportunidad. Ha sido una etapa llena de aprendizajes y retos constantes, donde he podido fortalecer mis conocimientos en diferentes áreas. Todo esto ha contribuido a mi crecimiento profesional y también me ha ayudado mucho en diferentes aspectos.",
+        name: "Elkin Sebastián Sanceno Garibello",
+    },
+    {
+        rating: 4.7,
+        text: "En mi experiencia Heinsohn ha sido muy enriquecedora tanto a nivel profesional como personal. Durante mi tiempo en la empresa, he tenido la oportunidad de trabajar en proyectos desafiantes y me han permitido fortalecer mis habilidades que me permitieron aplicar los conocimientos adquiridos en mi formación académica. Además, recibí acompañamiento constante por parte de profesionales de mi área y de paso pude aprender y tener una mejor habilidad en ese manejo de temas que no tenía previos conocimientos del cual se me hizo divertido e interesante.",
+        name: "Santiago Alexander García Soto",
+    },
+    {
+        rating: 4.8,
+        text: "Mi experiencia con heinsohn ha Sido el hecho de enfrentarme a una vida laboral, de una forma amena, en un ambiente amigable. Estoy agradecida día a día con heinsohn por abrirme sus puertas porque he aprendido tanto, a nivel empresarial y a nivel personal. Mi experiencia en heinsohn ha Sido mágica, en dónde me he enfrentado a retos que no pensé que la vida me fuera a colocar y aun así, me han dado la confianza y comodidad de entender aprender y crecer para mí y por mí, todo esto de forma dinámica, divertida y enriquecedora enamorándome cada día más de lo que hago.",
+        name: "Zaira Milena Rentería Cortes",
     },
 ];
 
